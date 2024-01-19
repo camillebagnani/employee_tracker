2d6754593c1f80dbf94415d61de96ac51934d380
camillebagnani <140850865+camillebagnani@users.noreply.github.com>
2024-01-19 12:36:59 -0700

    initialized mysql2 and connected to db

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'employees_db'
+    }
+);
 
 const prompts = [
     {
@@ -11,29 +21,48 @@ const prompts = [
 
 const init = () => {
     inquirer.prompt(prompts)
-    .then(({choice}) => {
-        if(choice === 'View All Departments') {
-            console.log('Test!')
-        } else if (choice === 'View All Roles') {
+        .then(({ choice }) => {
+            if (choice === 'View All Departments') {
+                db.connect(function (err) {
+                    if (err) throw err;
+                    db.query("SELECT * FROM department", function (err, results) {
+                        if (err) throw err;
+                        console.table(results);
+                        init();
+                    });
+                });
+            } else if (choice === 'View All Roles') {
+                db.connect(function (err) {
+                    if (err) throw err;
+                    db.query("SELECT * FROM role", function (err, results) {
+                        if (err) throw err;
+                        console.table(results);
+                    });
+                });
+            } else if (choice === 'View All Employees') {
+                db.connect(function (err) {
+                    if (err) throw err;
+                    db.query("SELECT * FROM employee", function (err, results) {
+                        if (err) throw err;
+                        console.table(results);
+                    });
+                });
+            } else if (choice === 'Add Department') {
 
-        } else if (choice === 'View All Employees') {
+            } else if (choice === 'Add Role') {
 
-        } else if (choice === 'Add Department') {
-        
-        } else if (choice === 'Add Role') {
-            
-        } else if (choice === 'Add Employee') {
+            } else if (choice === 'Add Employee') {
 
-        } else if (choice === 'Update Employee Role') {
+            } else if (choice === 'Update Employee Role') {
 
-        } else {
-            console.log('Successfully quit application')
-        }
-    })
-    .catch((error) => {
-        console.log(error);
-        console.log('Oops! Something went wrong.')
-    })
+            } else {
+                console.log('Use Control + C to quit application')
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log('Oops! Something went wrong.')
+        })
 }
 
 init();
